367967cd967bab30c5b49e5f5c9af0556502b981
Sasha1239 <ham1239@gmail.com>
2022-06-22 16:11:42 +0300

    fix: добавил валидации на поля класса Film

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -14,7 +14,9 @@ public class Film {
     private int id;
     @NotNull(message = "Наименование фильма не может быть пустым")
     private String name;
+    @NotNull(message = "Описание ильма не может быть пустым")
     private String description;
+    @NotNull(message = "Время релиза не может быть пустым")
     private LocalDate releaseDate;
     @Positive(message = "Продолжительность фильма должна быть положительной")
     private long duration;
